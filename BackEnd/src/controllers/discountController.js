e83fea3ef464d60a88b19bade8c1ab10abecbfb3
Ngo Van Toan <toango123zx@gmail.com>
2023-11-10 01:26:47 +0700

    Complete functionality to display discounts for suppliers or users by priority: start time, end time, value and end time

--- BackEnd/src/controllers/discountController.js
+++ BackEnd/src/controllers/discountController.js
@@ -17,3 +17,22 @@ export const getAllDiscount = async (req, res) => {
         });
     };
 };
+
+export const getDiscounts = async (req, res) => {
+    const __user = req.user;
+    let __discounts = await discountService.getDiscounts(__user.id_user);
+    if (!__discounts) {
+        return res.status(404).json({
+            position: "id user",
+            msg: "No discounts offered by this user were found"
+        });
+    } else {
+        __discounts = __discounts.map((discount) => {
+            discount.user = discount.user.role;
+            return discount;
+        });
+        return res.status(200).json({
+            data: __discounts
+        });
+    };
+};
--- BackEnd/src/routes/discount.js
+++ BackEnd/src/routes/discount.js
@@ -2,7 +2,9 @@ const express = require('express');
 const discountRouter = express.Router();
 
 import * as discountController from '../controllers/discountController';
+import * as authMiddleware from '../middlewares/authMiddleware';
 
 discountRouter.get('/all', discountController.getAllDiscount);
+discountRouter.get('/', authMiddleware.verifyToken, authMiddleware.checkSupplierOrAdminRole, discountController.getDiscounts);
 
 module.exports = discountRouter;
