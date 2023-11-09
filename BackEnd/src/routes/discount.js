8b15ccd682a32c79cea52f9a58ee0cc9a4b97a63
Ngo Van Toan <toango123zx@gmail.com>
2023-11-10 01:31:22 +0700

    Complete - see discount details - all users

--- BackEnd/src/routes/discount.js
+++ BackEnd/src/routes/discount.js
@@ -6,5 +6,6 @@ import * as authMiddleware from '../middlewares/authMiddleware';
 
 discountRouter.get('/all', discountController.getAllDiscount);
 discountRouter.get('/', authMiddleware.verifyToken, authMiddleware.checkSupplierOrAdminRole, discountController.getDiscounts);
+discountRouter.get('/:id', discountController.getDetailDiscount);
 
 module.exports = discountRouter;
--- BackEnd/src/services/discountService.js
+++ BackEnd/src/services/discountService.js
@@ -90,3 +90,39 @@ export const getDiscounts = async (id_user) => {
         return false;
     };
 };
+
+export const getDetailDiscount = async (id_discount) => {
+    try {
+        return prisma.discount.findFirst({
+            select: {
+                id_discount: true,
+                id_product: true,
+                name: true,
+                start_time: true,
+                description: true,
+                end_time: true,
+                value: true,
+                quantity: true,
+                point: true,
+                applited: true,
+                status: true,
+                user: {
+                    select: {
+                        role: true,
+                    }
+                }
+            },
+            where: {
+                id_discount: Number(id_discount),
+                status: {
+                    not: "cancel"
+                },
+                end_time: {
+                    gt: new Date(),
+                }
+            }
+        });
+    } catch (e) {
+        return false;
+    };
+};
