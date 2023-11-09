05737ad131b71c69ddbaa63e2a5e60be0fc3b253
Ngo Van Toan <toango123zx@gmail.com>
2023-11-10 01:42:43 +0700

    Completed - create new discount - verified

--- BackEnd/src/routes/discount.js
+++ BackEnd/src/routes/discount.js
@@ -7,5 +7,6 @@ import * as authMiddleware from '../middlewares/authMiddleware';
 discountRouter.get('/all', discountController.getAllDiscount);
 discountRouter.get('/', authMiddleware.verifyToken, authMiddleware.checkSupplierOrAdminRole, discountController.getDiscounts);
 discountRouter.get('/:id', discountController.getDetailDiscount);
+discountRouter.post('/', authMiddleware.verifyToken, authMiddleware.checkSupplierOrAdminRole, discountController.createDiscount);
 
 module.exports = discountRouter;
