848aa330689bd755ed144348363c5f728b5a1a5c
dinhtam1 <villadiwc123@gmail.com>
2023-11-16 16:09:43 +0700

    doc: Authentication
    doc: Authentication
    
    setup-swagger

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -7,22 +7,43 @@ const path = require('path');
 const app = express();
 const compression = require('compression')
 import * as envApp from './config/envApp';
-
 const route = require('./routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const options = {
+  definition: {
+    openapi: "3.0.3",
+    info: {
+      title: "Travalid API",
+      version: "1.0.1"
+    },
+    servers: [
+      {
+        url: `http://localhost:${envApp.port}`
+      }
+    ]
+  },
+  apis: ['./src/common/swagger/*.yaml'], // files containing annotations as above
+};
+const openapiSpecification = swaggerJsdoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+
+
 
 app.use(express.static(path.join(__dirname, '../public')));
 console.log(path.join(__dirname, '../public'));
 // HTTP Logger
 app.use(morgan('combined'));
 
-app.use(compression())
-
+// Allows the server to identify origin (protocol + domain name + port) to download and use resources from the server  
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
