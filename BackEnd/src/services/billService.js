e8279e07c31ef3abf718f104127803a12c52e1d6
itsvhagar <vietnguyennho626@gmail.com>
2024-01-01 15:44:38 +0700

    Get All Bills

--- BackEnd/src/services/billService.js
+++ BackEnd/src/services/billService.js
@@ -187,105 +187,113 @@ export const getBill = async (id_user) => {
     };
 };
 
+export const getAllBills = async () => {
+    try {
+        return await prisma.bill.findMany({
+            select: {
+                id_bill: true,
+                user: {
+                    select: {
+                        id_user: true,
+                        username: true,
+                        name: true,
+                        email: true,
+                        image: true,
+                    }
+                },
+                time: true,
+                quantity: true,
+                status: true,
+                info_bill: {
+                    select: {
+                        id_info_bill: true,
+                        schedule_product: {
+                            select: {
+                                start_time: true,
+                                end_time: true,
+                                price: true,
+                                status: true,
+                                product: {
+                                    select: {
+                                        name: true,
+                                        location: {
+                                            select: {
+                                                display_name: true,
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    } catch (e) {
+        return false;
+    };
+};
+
 // export const getAllBills = async () => {
-//     try {
-//         return await prisma.bill.findMany({
-//             select: {
-//                 id_bill: true,
-//                 id_user: true,
-//                 time: true,
-//                 quantity: true,
-//                 status: true,
-//                 info_bill: {
-//                     select: {
-//                         id_info_bill: true,
-//                         schedule_product: {
-//                             select: {
-//                                 start_time: true,
-//                                 end_time: true,
-//                                 price: true,
-//                                 status: true,
-//                                 product: {
-//                                     select: {
-//                                         name: true,
-//                                         location: {
-//                                             select: {
-//                                                 display_name: true,
-//                                             }
-//                                         }
-//                                     }
+//     return [
+//             {
+//                 "id_bill": 1,
+//                 "user": {
+//                     "name": "traveller1",
+//                     "email": "traveller1@gmail.com",
+//                     "image": "https://img.freepik.com/premium-psd/3d-cartoon-man-smiling-portrait-isolated-transparent-background-png-psd_888962-1570.jpg"
+//                 },
+//                 "time": "2023-12-21T11:22:47.020Z",
+//                 "quantity": 4,
+//                 "status": "pending",
+//                 "info_bill": [
+//                     {
+//                         "id_info_bill": 3184,
+//                         "schedule_product": {
+//                             "start_time": "2024-10-20T23:00:00.000Z",
+//                             "end_time": "2024-11-22T13:00:00.000Z",
+//                             "price": 1000000,
+//                             "status": "active",
+//                             "product": {
+//                                 "name": "Núi Bà Nà",
+//                                 "location": {
+//                                     "display_name": "Bà Nà Hill"
 //                                 }
 //                             }
 //                         }
 //                     }
-//                 }
-//             }
-//         });
-//     } catch (e) {
-//         return false;
-//     };
+//                 ]
+//             },
+//             {
+//                 "id_bill": 2,
+//                 "user": {
+//                     "name": "traveller2",
+//                     "email": "traveller2@gmail.com",
+//                     "image": "https://reputationprotectiononline.com/wp-content/uploads/2022/04/78-786207_user-avatar-png-user-avatar-icon-png-transparent.png"
+//                 },
+//                 "time": "2023-10-20T02:14:56.000Z",
+//                 "quantity": 8,
+//                 "status": "paided",
+//                 "info_bill": [
+//                     {
+//                         "id_info_bill": 3183,
+//                         "schedule_product": {
+//                             "start_time": "2024-11-22T23:00:00.000Z",
+//                             "end_time": "2024-11-24T13:00:00.000Z",
+//                             "price": 1500000,
+//                             "status": "active",
+//                             "product": {
+//                                 "name": "Núi Bà Nà",
+//                                 "location": {
+//                                     "display_name": "Bà Nà Hill"
+//                                 }
+//                             }
+//                         }
+//                     }
+//                 ]
+//             },
+//         ]
 // };
-
-export const getAllBills = async () => {
-    return [
-            {
-                "id_bill": 1,
-                "user": {
-                    "name": "traveller1",
-                    "email": "traveller1@gmail.com",
-                    "image": "https://img.freepik.com/premium-psd/3d-cartoon-man-smiling-portrait-isolated-transparent-background-png-psd_888962-1570.jpg"
-                },
-                "time": "2023-12-21T11:22:47.020Z",
-                "quantity": 4,
-                "status": "pending",
-                "info_bill": [
-                    {
-                        "id_info_bill": 3184,
-                        "schedule_product": {
-                            "start_time": "2024-10-20T23:00:00.000Z",
-                            "end_time": "2024-11-22T13:00:00.000Z",
-                            "price": 1000000,
-                            "status": "active",
-                            "product": {
-                                "name": "Núi Bà Nà",
-                                "location": {
-                                    "display_name": "Bà Nà Hill"
-                                }
-                            }
-                        }
-                    }
-                ]
-            },
-            {
-                "id_bill": 2,
-                "user": {
-                    "name": "traveller2",
-                    "email": "traveller2@gmail.com",
-                    "image": "https://reputationprotectiononline.com/wp-content/uploads/2022/04/78-786207_user-avatar-png-user-avatar-icon-png-transparent.png"
-                },
-                "time": "2023-10-20T02:14:56.000Z",
-                "quantity": 8,
-                "status": "paided",
-                "info_bill": [
-                    {
-                        "id_info_bill": 3183,
-                        "schedule_product": {
-                            "start_time": "2024-11-22T23:00:00.000Z",
-                            "end_time": "2024-11-24T13:00:00.000Z",
-                            "price": 1500000,
-                            "status": "active",
-                            "product": {
-                                "name": "Núi Bà Nà",
-                                "location": {
-                                    "display_name": "Bà Nà Hill"
-                                }
-                            }
-                        }
-                    }
-                ]
-            },
-        ]
-};
 
 export const createBill = async (bill) => {
     try {
